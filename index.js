13d2d131adf2ab5ee9093c72f3ddf63d2bc8913b
khadija-0 <khadija.dahir@student.moringaschool.com>
2022-09-07 10:50:28 +0300

    completed

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
-}
+    static capitalize(string) {
+      return `${string[0].toUpperCase()}${string.slice(1)}`
+    }
+  
+    static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9 '-]/g, '')
+    }
+  
+    static titleize(string) {
+      let stringArr = string.split(" ")
+      let newString = []
+      let lowerCaseString = ["a", "an", "but", "of", "and", "for", "at", "by", "the", "from"]
+      stringArr.forEach(function (word) {
+        if (!lowerCaseString.includes(word)) {
+          newString.push(`${word[0].toUpperCase()}${word.slice(1)}`)
+        } else {
+          newString.push(word)
+        }
+      })
+      return `${newString[0][0].toUpperCase()}${newString[0].slice(1)} ${newString.slice(1).join(" ")}`
+    }
+  }
+  
+  
+  
+  Formatter.titleize("chicken soup with rice and a few other songs")
+  Footer
+  
